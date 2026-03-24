28614aaac0fcdce8af12d750b7192199c9f20085
ImFredy <fredyyousef06@gmail.com>
2026-03-24 16:07:54 -0400

    testing commit

--- Solution.py
+++ Solution.py
@@ -18,6 +18,7 @@ class Solution:
         give them is longer than the shortest path from the source. routing delay is the distance from A to B
         so we just have to do BFS from source to client, we also have inf bandwidth so we just gotta give the path
         """
+
         paths, bandwidths, priorities = {}, {}, {}
         # Note: You do not need to modify all of the above. For Problem 1, only the paths variable needs to be modified. If you do modify a variable you are not supposed to, you might notice different revenues outputted by the Driver locally since the autograder will ignore the variables not relevant for the problem.
         # WARNING: DO NOT MODIFY THE LINE BELOW, OR BAD THINGS WILL HAPPEN

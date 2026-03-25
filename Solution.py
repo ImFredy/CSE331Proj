3e2bed17509010c0d730aa8e6c2cb6c5ae523c15
ImFredy <fredyyousef06@gmail.com>
2026-03-24 21:50:19 -0400

    utilities.py had info[list_clients] as the client list so ill see if that works if not ill try just putting self.info[list_clients] or a loop to get their ids

--- Solution.py
+++ Solution.py
@@ -18,7 +18,7 @@ class Solution:
         give them is longer than the shortest path from the source. routing delay is the distance from A to B
         so we just have to do BFS from source to client, we also have inf bandwidth so we just gotta give the path
         """
-        clients = list(self.info.keys()) #since self.info is a dict c:rev the list of clients is the list of keys - FY
+        clients = list(self.info["list_clients"]) #correction this will give me the client ids so ill submit this since it keeps saying im giving strings and not int. I found this in utlitly.py checking if it works - Fy
         paths = bfs_path(self.graph, self.isp, clients) #Bfs path will take the graph, use isp as starting node and find a path for every client, going to submit -FY
         bandwidths, priorities = {}, {}
         # Note: You do not need to modify all of the above. For Problem 1, only the paths variable needs to be modified. If you do modify a variable you are not supposed to, you might notice different revenues outputted by the Driver locally since the autograder will ignore the variables not relevant for the problem.

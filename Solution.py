856ff0fe8d4152e34424353c725a5a5b480bebfb
ImFredy <fredyyousef06@gmail.com>
2026-03-27 19:40:46 -0400

    finished a dijkstras function that i think will work for what we need wrote what we have to do in solution.py - FY

--- Solution.py
+++ Solution.py
@@ -19,10 +19,21 @@ class Solution:
         so we just have to do BFS from source to client, we also have inf bandwidth so we just gotta give the path
         """
         """
-        Prob 2: going to submit our answer for p1 to see where we at commiting now for fresh start
+        Prob 2: revenue is off, but here is what we have to do: for each of the clients we can run bfs to find the shortest delay
+        then we should give each client a deadline to help us find out the max delay for each client. we also need to have a way to track
+        how many packets are in a node to get rid of congestion like pn=0. ill try to sort the clients by higher priority being our rev first and if that
+        doesnt work then we can start with the most needy clients. if we use dijstras algo we can find all the paths for the packets and then if we set the edges weights outselfs 
+        we can control the flow of dijstras which controls the congestion and nodes that are at their max bandwidth from clogging.
         """
-        clients = list(self.info["list_clients"]) #correction this will give me the client ids so ill submit this since it keeps saying im giving strings and not int. I found this in utlitly.py checking if it works - Fy
-        paths = bfs_path(self.graph, self.isp, clients) #Bfs path will take the graph, use isp as starting node and find a path for every client, going to submit -FY
+        graph = self.graph #added these for QOL - FY
+        isp = self.isp
+        info = self.info
+        clients = list(info["list_clients"]) #correction this will give me the client ids so ill submit this since it keeps saying im giving strings and not int. I found this in utlitly.py checking if it works - Fy
+        bandwidths = info["bandwidths"]
+
+        #dijkalgo done but not tested and need to use it to get the paths, also need to implement the packet load tracking and update it as we assign paths to clients - Fy
+
+        paths = bfs_path(graph, isp, clients) #Bfs path will take the graph, use isp as starting node and find a path for every client, going to submit -FY
         bandwidths, priorities = {}, {}
         # Note: You do not need to modify all of the above. For Problem 1, only the paths variable needs to be modified. If you do modify a variable you are not supposed to, you might notice different revenues outputted by the Driver locally since the autograder will ignore the variables not relevant for the problem.
         # WARNING: DO NOT MODIFY THE LINE BELOW, OR BAD THINGS WILL HAPPEN
--- Traversals.py
+++ Traversals.py
@@ -25,3 +25,43 @@ def bfs_path(graph, isp, list_clients):
         paths[client] = path
 
     return paths
+
+def dijkalgo(graph,isp,list_clients,bandwidths,pn):
+    #graph is the graph, isp is the source, list_clients is the list of clients, bandwidths is the bandwidth of each node, pn the packet load in the node - fy
+    paths={}
+    for client in list_clients:
+        cost = {}
+        nodefrom = {}
+        visited = {}
+        for node in graph:
+            cost[node] = "N/A" #these nodes are unreachable until a path is assinged - Fy
+            nodefrom[node] = None #this is the node that the path will come from, we can use this to backtrack and find the path - Fy
+            visited[node] = False #this is to keep track of which nodes we have visited - Fy
+        cost[isp] = 0 #the cost of the source node is 0 - Fy
+        while True:
+            current = None
+            for node in graph:
+                if not visited[node] and cost[node]!="N/A": #if the node is not visited and is reachable - Fy
+                    if current is None or cost[node] < cost[current]: #if the current node is None or the cost of the node is less than the cost of the current node, we update the current node - Fy
+                     current = node
+            if current is None or current == client: #if there are no more reachable nodes, we break - Fy
+                break
+            visited[current] = True
+            for neighbor in graph[current]:
+                if not visited[neighbor]:
+                    band = bandwidths[neighbor]
+                    if pn[neighbor] < band: #if the packet load in the neighbor is less than the bandwidth of the neighbor, we can consider this path - Fy
+                        newcost = cost[current] + 1 + (pn.get(neighbor, 0)/band)**2 #cost to get here + 1 to go to neighbor + a squared penalty based on how full the neighbor is, so almost full nodes become very expensive - Fy
+                        if cost[neighbor] == "N/A" or newcost < cost[neighbor]: #if the neighbor is unreachable or the new cost is less than the current cost of the neighbor, we update the cost and nodefrom - Fy
+                            cost[neighbor] = newcost
+                            nodefrom[neighbor] = current
+        path = []
+        node = client
+        while node is not None: #we backtrack from the client to the source to find the path - Fy
+            path.insert(0,node)
+            node = nodefrom[node]
+        if path and path[0] == isp: #if the path is not empty and starts with the source, we add it to the paths - Fy
+            paths[client] = path
+        else:
+            paths[client] = [] #if the path is empty or does not start with the source, we set it to an empty path - Fy
+    return paths
